c5ad8cd763a2a410186a54058296a4dbce23bb22
crisnicogonzalez <cristian3629@gmail.com>
2018-09-27 22:44:45 -0300

    US-0002-02 - add redirect service and loginComponent

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route,Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route,Switch,Redirect } from "react-router-dom";
 import AdminContainer from './tabs/admin/AdminContainer'
 import TeacherContainer from './tabs/teacher/TeacherContainer';
-import HomeContainer from './tabs/home/HomeContainer';
 import NotFound from '../src/common/NotFound'
 import AdminDepContainer from '../src/tabs/admin-dep/AdminDepContainer'
+import LoginContainer from './tabs/login/LoginContainer'
 
 const App = () => (
-  <Router>
+  <Router basename="siu-guarani-web">
     <Switch>
-      <Route exact path="/" component={HomeContainer} />
+      <Route path="/login" component={LoginContainer}/>
+      <Route path="/" render={() => <Redirect to="/login" />}/>
       <Route path="/admin" component={AdminContainer} />
       <Route path="/teacher" component={TeacherContainer} />
       <Route path="/admin-dep" component={AdminDepContainer} />
       <Route component={NotFound}/>
-    </Switch>
+    </Switch>  
   </Router>
 );
 
---
+++ src/tabs/login/LoginContainer.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+
+
+class LoginContainer extends Component {
+  render(){
+    return <h1>Login Container asaa</h1>
+  }
+}
+
+export default LoginContainer;

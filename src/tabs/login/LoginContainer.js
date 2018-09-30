a95c87d947b825c270551a5b02606064165e8f38
crisnicogonzalez <cristian3629@gmail.com>
2018-09-30 02:50:15 -0300

    US-0002-02 - error message when the user or password are incorrect

--- src/tabs/login/LoginContainer.js
+++ src/tabs/login/LoginContainer.js
@@ -7,39 +7,23 @@ import {Redirect} from "react-router-dom";
 class LoginContainer extends Component {
 
   state = {
-    loginButtonLoading:false
-  }
-
-  userIsLogged = () => {
-    return localStorage.getItem('rol') !== null;
+    userIsLogged:false,
   }
 
   getRender = () => {
-    if(this.userIsLogged()){
+    if(this.state.userIsLogged){
       return <Redirect to="/" />
     }
     return <WrappedNormalLoginForm 
-      loginUser = {this.handlerAuth}
-      loginButtonLoading={this.state.loginButtonLoading}
-    />
+            userLogged={this.userLogged}
+            />
+  }
+
+  userLogged = () => {
+    this.setState({userIsLogged:true})
   }
 
 
-
-  handlerAuth = (authInfo) =>{
-    AuthService.authUser(authInfo).then((response)=>{
-      console.log('LoginContainer - the user is logged');  
-      console.log('LoginContainer - response',response);
-      this.setState({loginButtonLoading:false})
-      localStorage.setItem('token',response.data.token);
-      localStorage.setItem('rol',response.data.rol)
-      this.setState({userIsLogged:true})
-    }).catch((e)=>{
-      console.log('LoginContainer -  the is not logged');
-      console.log('LoginContainer -  error',e);
-      
-    })
-  }
   
   render() {
     return this.getRender();

2993ebb32b491dd58fcd5a6b85e1b1d07825dec5
crisnicogonzalez <cristian3629@gmail.com>
2018-09-30 14:21:03 -0300

    Change server config

--- src/Server.js
+++ src/Server.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosIntance = axios.create({
-  baseURL:'http://localhost:8080/api/v1.0',
+  baseURL:'http://localhost:3000/api/v1.0',
 })
 
 export default axiosIntance;
